2a5cc447594129f38bdaad1eb0fbc85cc9b59c30
Frederick <frederick.wennborg@gmail.com>
2023-03-05 14:44:52 +0100

    finished js implementation with f-products

--- js/script.js
+++ js/script.js
@@ -1,3 +1,43 @@
-// document.getElementById("toggle-menu-button").addEventListener("click", () => {
-//     document.getElementById("menu-icons-links").classList.add("d-flex");
-// })
+async function getProducts(target, tag) {
+    const element = document.querySelector(target)
+
+    const res = await fetch(`https://kyh-net22.azurewebsites.net/api/products/${tag}`)
+    const data = await res.json()
+
+    for (let item of data) {
+        element.innerHTML +=
+       
+        `
+        <div class="product-card">
+            <div class="card-content">
+                <img src="${item.imageUrl}" alt="${item.name}">
+                <div class="card-menu">
+                    <nav class="icons">
+                        <a class="link" href="#"><i class="fa-regular fa-heart"></i></a>
+                        <a class="link" href="#"><i class="fa-regular fa-bag-shopping"></i></a>
+                    </nav>
+              <a class="btn-theme" href="#">QUICK VIEW</a>
+            </div>
+            <div class="card-body">
+              <p class="category">${item.category}</p>
+              <p class="title">${item.name}</p>
+              <div class="ranking">
+                <i class="fa-solid fa-sharp fa-star"></i>
+                <i class="fa-solid fa-sharp fa-star"></i>
+                <i class="fa-solid fa-sharp fa-star"></i>
+                <i class="fa-solid fa-sharp fa-star"></i>
+                <i class="fa-regular fa-sharp fa-star"></i>
+              </div>
+              <p class="price">${item.originalPrice} ${item.currency}</p>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    </div>       
+         
+        `
+     
+    }
+}
