614e6abbf2a11b53f66a257cfec5d44f57f917fc
Frederick <frederick.wennborg@gmail.com>
2023-03-07 16:35:08 +0100

    added js for header button toggle

--- js/script.js
+++ js/script.js
@@ -15,7 +15,39 @@ window.addEventListener('scroll', function () {
 
 })
 
+window.addEventListener('scroll', function () {
+  const scrollPosY = window.scrollY
 
+  if (scrollPosY >= 100) {
+      arrow.style.display = 'block'
+  }
+  else {
+      arrow.style.display = 'none'
+  }
+
+})
+
+try {
+  const toggleButton = document.querySelector('[data-option="toggle"]')
+  const target = toggleButton.getAttribute('data-target')
+  toggleButton.addEventListener('click', toggleTarget)
+
+
+  function toggleTarget() {
+      const element = document.querySelector(target)
+
+      if (!element.classList.contains('hide')) {
+          element.classList.add('hide')
+      }
+      else {
+          element.classList.remove('hide')
+      }
+
+  }
+
+
+}
+catch { }
 
 
 
