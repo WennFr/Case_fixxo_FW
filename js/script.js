207d377b2881ffe0286520cd43770da1f28d62b6
Frederick <frederick.wennborg@gmail.com>
2023-03-13 14:15:50 +0100

    implemented handlePostComment function that validates the post function of the form and communicates with a local API.

--- js/script.js
+++ js/script.js
@@ -3,17 +3,17 @@
 
 const arrow = document.querySelector('#top-arrow')
 arrow.addEventListener('click', function () {
-    window.scrollTo({ top: 0, behavior: "smooth" })
+  window.scrollTo({ top: 0, behavior: "smooth" })
 })
 
 window.addEventListener('scroll', function () {
   const scrollPosY = window.scrollY
 
   if (scrollPosY >= 100) {
-      arrow.style.display = 'block'
+    arrow.style.display = 'block'
   }
   else {
-      arrow.style.display = 'none'
+    arrow.style.display = 'none'
   }
 
 })
@@ -22,10 +22,10 @@ window.addEventListener('scroll', function () {
   const scrollPosY = window.scrollY
 
   if (scrollPosY >= 100) {
-      arrow.style.display = 'block'
+    arrow.style.display = 'block'
   }
   else {
-      arrow.style.display = 'none'
+    arrow.style.display = 'none'
   }
 
 })
@@ -39,14 +39,14 @@ try {
 
 
   function toggleTarget() {
-      const element = document.querySelector(target)
+    const element = document.querySelector(target)
 
-      if (!element.classList.contains('hide')) {
-          element.classList.add('hide')
-      }
-      else {
-          element.classList.remove('hide')
-      }
+    if (!element.classList.contains('hide')) {
+      element.classList.add('hide')
+    }
+    else {
+      element.classList.remove('hide')
+    }
 
   }
 
@@ -58,15 +58,15 @@ catch { }
 // Products-fetch
 
 async function getProducts(target, tag) {
-    const element = document.querySelector(target)
+  const element = document.querySelector(target)
 
-    const res = await fetch(`https://kyh-net22.azurewebsites.net/api/products/${tag}`)
-    const data = await res.json()
+  const res = await fetch(`https://kyh-net22.azurewebsites.net/api/products/${tag}`)
+  const data = await res.json()
 
-    for (let item of data) {
-        element.innerHTML +=
-       
-        `
+  for (let item of data) {
+    element.innerHTML +=
+
+      `
         <div class="product-card">
             <div class="card-content">
                 <img src="${item.imageUrl}" alt="${item.name}">
@@ -100,88 +100,157 @@ async function getProducts(target, tag) {
     </div>       
          
         `
-     
-    }
+
+  }
 }
 
 
 // validate contact form
 
 
-function validate(event){
+function validate(event) {
 
   switch (event.target.id) {
 
     case 'name':
-      
-          validateName(event.target)
-          break;
 
-      case 'email':
-          validateEmail(event.target)
-          break;
+      validateName(event.target)
+      break;
 
-      case 'comments':
-          validateComment(event.target)
-          break;
-    
-  
+    case 'email':
+      validateEmail(event.target)
+      break;
+
+    case 'comments':
+      validateComment(event.target)
+      break;
+
+
   }
 
 }
 
-
-
-function validateName(element){
+function validateName(element) {
   const regEx = /^[A-Za-z\u00C0-\u017F]+(?:[-\s][A-Za-z\u00C0-\u017F]+)?(?:[-\s][A-Za-z\u00C0-\u017F]+)?$/;
   const errorElement = document.getElementById(`error-${element.id}`)
 
-  if (!regEx.test(element.value)){
+  if (!regEx.test(element.value)) {
 
-      errorElement.innerHTML = `must be a valid name`
-      return false
+    errorElement.innerHTML = `must be a valid name`
+    return false
 
   }
-  
+
   errorElement.innerHTML = ``
   return true
 
 }
 
 
-function validateEmail(element){
+function validateEmail(element) {
   const regEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   const errorElement = document.getElementById(`error-${element.id}`)
 
-  if (!regEx.test(element.value)){
+  if (!regEx.test(element.value)) {
 
-      errorElement.innerHTML = `must be a valid email address`
-      return false
+    errorElement.innerHTML = `must be a valid email address`
+    return false
 
   }
-  
+
   errorElement.innerHTML = ``
   return true
 
 }
 
-function validateComment(element){
+function validateComment(element) {
   const regEx = /^(?=.*[A-Za-z])[A-Za-z0-9\såäöÅÄÖ!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]{1,500}$/
   const errorElement = document.getElementById(`error-${element.id}`)
 
-  if (!regEx.test(element.value)){
+  if (!regEx.test(element.value)) {
 
-      errorElement.innerHTML = `must be a valid comment`
-      return false
+    errorElement.innerHTML = `must be a valid comment`
+    return false
 
   }
-  
+
   errorElement.innerHTML = ``
   return true
 
 }
 
+async function handlePostComment(e) {
+  e.preventDefault()
+  const errors = []
+  const errorMessage = document.getElementById('errorMessage')
+  errorMessage.innerHTML = ''
+
+  for (let element of e.target) {
+
+    if (element.required) {
+      const errorElement = document.getElementById(`error-${element.id}`)
+
+      if (element.value.length === 0) {
+        errorElement.innerHTML = `${element.id} is required.`
+        errors.push(false)
+      }
+      else {
+        errorElement.innerHTML = ``
+
+        switch (element.type.id) {
+          case 'name':
+            errors.push(validateName(element))
+            break;
+          case 'email':
+            errors.push(validateEmail(element))
+            break;
+          case 'comments':
+            errors.push(validateComment(element))
+            break;
+
+        }
+      }
+    }
+  }
 
 
+  if (!errors.includes(false)) {
+
+    const form = {
+      name: e.target['name'].value,
+      email: e.target['email'].value,
+      comments: e.target['comments'].value
+
+    }
+
+    const res = await fetch('https://localhost:7046/api/Login',{
+    method: 'post',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(form)
+
+    })
+
+    //sparar nyckel i cookies
+    if(res.status === 200){
+        const result = await res.text()
+        sessionStorage.setItem('accessToken', result)
+        errorMessage.innerHTML = 'Tack för din förfrågan!'
+    } else{
+    errorMessage.innerHTML = 'Incorrect name, email or comment'
+
+    }
+
+
+}
+
+
+
+
+
+
+
+}
 
 
